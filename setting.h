0d235c0adb80299620ea2a370e74967a48ba8a49
phil <peychart@mail.pf>
2019-12-08 17:42:51 -1000

    display counter text on the failure of SteelSeries-Canvas

--- setting.h
+++ setting.h
@@ -1,6 +1,6 @@
 //Ajust the following:
 
-unsigned short version =        1;                  //Change this value to reset current config on the next boot...
+#define VERSION                "1.0.1"              //Change first number to reset current config on the next boot...
 #define DEFAULTHOSTNAME        "ESP8266"
 #define DEFAULTWIFIPASS        "defaultPassword"
 #define WIFISTADELAYRETRY       30000UL
@@ -39,4 +39,4 @@ unsigned short version =        1;                  //Change this value to reset
 
 #define BACKGROUND_IMAGE       "https://static.mycity.travel/manage/uploads/7/36/12705/989bd67a1aad43055bd0322e9694f8dd8fab2b43_1080.jpg"
 
-//#define DEBUG
+#define DEBUG
